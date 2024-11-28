b5d58bcbd2b0425668a25a38a63c174adbe21011
Vincent <contact@vincent-robert.com>
2024-11-28 16:03:07 +0100

    Add original author details to new issue

--- app/overrides/issues/_action_menu.rb
+++ app/overrides/issues/_action_menu.rb
@@ -1,12 +1,4 @@
 Deface::Override.new :virtual_path => 'issues/_action_menu',
                      :name => 'add-link-issue-button-to-issue-actions',
                      :insert_before => 'erb[loud]:contains("link_to l(:button_edit)")',
-                     :text => <<~RELATEDISSUE
-                       <% if Setting["plugin_redmine_related_issue_shortcut"]["create_related_issue_shortcut"].present? %>
-                           <%= link_to l(:label_create_related_issue_shortcut),
-                               new_project_issue_path(project_id: Setting["plugin_redmine_related_issue_shortcut"]["create_related_issue_shortcut_project_id"] || @project.id,
-                                                      'issue[description]' =>  @issue.description,
-                                                      related_to: @issue,),
-                               class: "icon icon-link-break" %>
-                       <% end %>
-                     RELATEDISSUE
+                     :partial => 'issues/link_to_new_related_issue'
