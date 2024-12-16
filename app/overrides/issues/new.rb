c338f72569fd3aab6255b3858fef68b0205c05b7
Vincent <contact@vincent-robert.com>
2024-12-16 14:26:14 +0100

    Simplify form: remove useless save-and-continue button

--- app/overrides/issues/new.rb
+++ app/overrides/issues/new.rb
@@ -2,3 +2,7 @@ Deface::Override.new :virtual_path  => "issues/new",
                      :name          => "add-related-issue-if-any",
                      :insert_before => "div.box.tabular",
                      :partial       => "issues/related_issue"
+
+Deface::Override.new :virtual_path  => "issues/new",
+                     :name          => "remove_create_and_continue_button",
+                     :remove        => "erb[loud]:contains(\"submit_tag l(:button_create_and_continue)\")"
